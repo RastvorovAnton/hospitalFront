261b5160c78de701d1b2fb887cb84b72351c0cb1
Anton Rastvorov <antonrostvorov.exceedteam@gmail.com>
2022-03-31 10:19:44 +0300

    fix comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RegistrationPage from './components/Pages/RegistrationPage';
+// import AuthorizationPage from './components/Pages/AuthorizationPage';
 import Header from './components/RegistrationPage/Header/Header';
+// import MainPage from './components/MainPage/MainPage';
+// import Table from './././components/MainPage/Table';
 import './App.css';
 
 const App = () => {
@@ -12,6 +15,9 @@ const App = () => {
 
         <Routes>
           <Route path='/registration' element={<RegistrationPage />} />
+          {/* <Route path='/authorization' element={<AuthorizationPage />} />
+          <Route path='/mainPage' element={<MainPage />} />
+          <Route path='/table' element={<Table />} /> */}
           <Route path='/' element={<RegistrationPage />} />
         </Routes>
 
